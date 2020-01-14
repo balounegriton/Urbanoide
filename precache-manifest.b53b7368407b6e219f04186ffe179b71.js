127b847d6e25ba0085ffe030cf0a65fb2018e0ca
balounegriton <provostnoe@gmail.com>
2020-01-14 12:35:09 -0500

    Updates

---
+++ precache-manifest.b53b7368407b6e219f04186ffe179b71.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba1007212adfacb4c7f97935ec6904e",
+    "url": "/Urbanoide/index.html"
+  },
+  {
+    "revision": "41a73cccf1f4ea536acd",
+    "url": "/Urbanoide/static/css/main.92239098.chunk.css"
+  },
+  {
+    "revision": "ba7f510bcade97aba392",
+    "url": "/Urbanoide/static/js/2.9126592b.chunk.js"
+  },
+  {
+    "revision": "9fcbc669d68edcb506b730c9ae8a8e55",
+    "url": "/Urbanoide/static/js/2.9126592b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41a73cccf1f4ea536acd",
+    "url": "/Urbanoide/static/js/main.5e8b3f99.chunk.js"
+  },
+  {
+    "revision": "84d56ee40275a5018d8b",
+    "url": "/Urbanoide/static/js/runtime-main.ef41f7bc.js"
+  },
+  {
+    "revision": "f2e01eddcc5fa011a0d5808f88c3ab69",
+    "url": "/Urbanoide/static/media/loop.f2e01edd.mp4"
+  },
+  {
+    "revision": "fd9f7b6a6d388d434a15e8715a6a5387",
+    "url": "/Urbanoide/static/media/urb_favicon_black.fd9f7b6a.svg"
+  },
+  {
+    "revision": "1d679ac45d52908213dcc4d5987209c3",
+    "url": "/Urbanoide/static/media/urb_logo_black.1d679ac4.svg"
+  }
+]);
