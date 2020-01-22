b9c978eaba1deb8b767b5cefb09d5456e5970365
balounegriton <provostnoe@gmail.com>
2020-01-22 14:50:57 -0500

    new version

--- src/App.js
+++ src/App.js
@@ -1,113 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import loop from "./video/loop.mp4";
-import img from "./video/img.jpg";
 import Anim1 from "./Animation/Amin1.js";
-import Anim2 from "./Animation/Amin2.js";
+import { ReactComponent as Connecting } from './connecting.svg';
+import { ReactComponent as Email } from './email.svg';
 import { withGetScreen } from 'react-getscreen'
-import VisibilitySensor from "react-visibility-sensor";
+
 
 
 class App extends Component {
   constructor() {
     super();
-    const me = this
     this.state = {
     };
-
-    var scrollStop = function (callback) {
-
-      // Make sure a valid callback was provided
-      if (!callback || typeof callback !== 'function') return;
-
-      // Setup scrolling variable
-      var isScrolling;
-
-      // Listen for scroll events
-      window.addEventListener('scroll', function (event) {
-
-        // Clear our timeout throughout the scroll
-        window.clearTimeout(isScrolling);
-
-        // Set a timeout to run after scrolling ends
-        isScrolling = setTimeout(function () {
-
-          // Run the callback
-          callback();
-
-        }, 666);
-
-      }, false);
-
-    };
-
-    scrollStop(function () {
-      me.pauseVideo()
-
-    });
-  }
-
-
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-    setTimeout(function(){ document.body.style.overflow = "scroll" }, 2500);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll = () => {
-    this.playVideo()
   }
 
 
   playVideo = () => {
-    
     if (this.props.isMobile() || this.props.isTablet()) {
-      document.getElementById("img1").className = "mouseHover2";
+      //  pourmobile a faire
     }
 
-    else{
-      this.refs.vidRef.play();
+    else {
       document.getElementById("video1").className = "mouseHover";
     }
-
-
-
   };
 
-  pauseVideo = () => {
 
-    if (this.props.isMobile() || this.props.isTablet()) {
-      document.getElementById("img1").className = "mouseNotHover";
-    }
+  mouseStop = () => {
+    document.getElementById("video1").className = "mouseNotHoverTop"
+    document.getElementById("logo").className = "logoView";
+    this.setState({ swap: !this.state.swap })
 
-    else{
-    this.refs.vidRef.pause();
-    document.getElementById("video1").className = "mouseNotHover";
-    }
-
-  };
-
-  onChange = (isVisible) => {
-
-    this.setState({
-      visable: isVisible
-    })
   }
 
-  onChange2 = (isVisible) => {
-   
-    this.setState({
-      visable2: isVisible
-    })
-  }
 
-  mail= () => {
-   
-    window.open('mailto:contact@urbanoid.com')
+
+  mouseMove = () => {
+    document.getElementById("video1").className = "mouseHover";
+    document.getElementById("logo").className = "logoHide";
+    this.setState({ visable: true })
+
+    clearTimeout(this.myTimeOut);
+
+    this.myTimeOut = setTimeout(this.mouseStop, 100);
+
   }
 
 
@@ -120,108 +57,63 @@ class App extends Component {
 
     if (this.props.isMobile() || this.props.isTablet()) {
       return (
-
-
-        <div>
-        <div className="allo2">        
-              <  Anim1  />  
+        <div id="theBody">
+          mobile tablet
         </div>
-
-        <div className="allo2">
-        <VisibilitySensor onChange={this.onChange2}>
-            <div className="emptydiv2">
-              {this.state.visable2 && <div className="anim2" onClick={this.mail}>     
-                
-              <  Anim2  />  </div>}
-            </div>
-
-          </VisibilitySensor>
-        </div>
-
-
-        <div className="colorBackground">
-          <img
-           id="img1"
-           className="mouseNotHover"
-           src={img}
-            alt=""
-            />
-  
-           
-         
-         
-      
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-      </div>
       )
-
-
 
     }
 
 
-
-    ////// Desktop version
+        ////// destop version
     else {
 
+
+
+
+
       return (
-        <div>
-          <div className="allo2">        
-                <  Anim1  />  
+        <div id="theBody" onMouseMove={this.mouseMove}>
+
+
+
+
+          {this.state.visable && <div className="fixeDiv">
+            <div className="anim2" onClick={this.mail} >
+
+              {!this.state.swap && <  Connecting />}
+              {this.state.swap && <  Email />}
+            </div>
+
+          </div>}
+
+
+
+
+
+          <div id="logo" className="logoView" >
+            <div className="anim1"><  Anim1 />
+            </div>
+
           </div>
-
-          <div className="allo2">
-          <VisibilitySensor onChange={this.onChange2}>
-              <div className="emptydiv2">
-                {this.state.visable2 && <div className="anim2" onClick={this.mail} >     
-                  
-                <  Anim2  />  </div>}
-              </div>
-
-            </VisibilitySensor>
-          </div>
-
 
           <div className="colorBackground">
             <video
               id="video1"
-              className="mouseNotHover"
+              className="mouseNotHoverTop"
               ref="vidRef"
               src={loop}
               type="video/mp4"
               loop
               muted
+              autoPlay
             />
           </div>
-
-
-
-
-
-
-
-
-
-
 
         </div>
       )
 
     }
-
-
-
   }
 }
 
